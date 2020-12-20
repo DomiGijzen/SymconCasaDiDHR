75ad317e783c818cf324906e45471b8ed807230b
Dominique Gijzen <Dominique@GijzenDMA.nl>
2020-12-20 15:18:38 +0100

    form en module

--- LightControl/module.php
+++ LightControl/module.php
@@ -5,6 +5,18 @@
 		{
 			//Never delete this line!
 			parent::Create();
+			
+			//Properties
+			$this -> RegisterPropertyInteger("DimmerType", 0)
+			$this -> RegisterPropertyInteger("DimmerInstance", 0)
+			$this -> RegisterPropertyInteger("DimmerKanaal", 0)
+			$this -> RegisterPropertyInteger("StartPercentage", 0)
+			$this -> RegisterPropertyInteger("StdPercentage", 50)
+			$this -> RegisterPropertyInteger("StdDimTime", 1)
+
+			//StatusVariables
+			$this->RegisterVariableBoolean("Status", "Lamp Aan/Uit", "~Switch");
+			$this->RegisterVariableInteger("Dim", "Lamp Dim stand", "~DimLedSpot");
 		}
 
 		public function Destroy()
