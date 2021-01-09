6debc6506a71a91ce0ae7f1806f4898dfd93a6c6
Dominique Gijzen <Dominique@GijzenDMA.nl>
2021-01-09 14:41:31 +0100

    Added PresenceDetectionModule

---
+++ PresenceDetection/module.php
@@ -0,0 +1,32 @@
+<?php
+	class LightControl extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+			//Properties
+
+			//StatusVariables
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		//Module Functions
+
+		public function RequestAction($Ident, $Value)
+		{
+		}
+
+	}
