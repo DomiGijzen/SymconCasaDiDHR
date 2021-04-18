5cdc205774f2f62c60fe2719e27593be5c541fce
Dominique Gijzen <Dominique@GijzenDMA.nl>
2021-04-18 11:18:38 +0200

    Added logic for motiondetection

--- MotionDetection/module.php
+++ MotionDetection/module.php
@@ -7,6 +7,8 @@
 			parent::Create();
 			
 			//Properties
+			$this -> RegisterPropertyInteger("MotionSensorType", 0);
+			$this -> RegisterPropertyInteger("Instance", 0);
 
 			//StatusVariables
 			$this -> RegisterVariableBoolean("MomentaryPresence", "Momentary presence", "~Presence", 10);
@@ -32,9 +34,67 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 
+			$sensors = $this -> ReadPropertyInteger('MotionSensors');
+		
+			//Update active sensors
+			$this -> updateActive();
+		
+			//Deleting all References
+            foreach ($this->GetReferenceList() as $referenceID) {
+                $this -> UnregisterReference($referenceID);
+			}
+			
+			//Adding references for targets
+            foreach ($sensors as $sensor) {
+                $this -> RegisterMessage($sensor -> VariableID, VM_UPDATE);
+                $this -> RegisterReference($sensor -> VariableID);
+            }
 		}
 
 		//Module Functions
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+        {
+			$sensors = $this->ReadPropertyInteger('MotionSensors');
+			
+			$this -> SendDebug('MessageSink', 'SenderID: ' . $SenderID . ', Message: ' . $Message, 0);
+
+            foreach ($sensors as $sensor) {
+                if ($sensor -> VariableID == $SenderID) {
+                    $this -> TriggerMotion($sensor -> VariableID, GetValue($sensor -> VariableID));
+                    $this -> updateActive();
+                    return;
+                }
+            }
+		}
+		
+        public function TriggerMotion(int $SourceID, $SourceValue)
+        {
+			SetValue($this -> GetIDForIdent('Motion'), True);
+			$this->SetTimerInterval("Delay", 5000);
+		}
+		
+		public function ResetMotion()
+		{
+            SetValue($this -> GetIDForIdent('Motion'), False);
+		}
+
+        private function updateActive()
+        {
+			$sensors = $this -> ReadPropertyString('MotionSensors');
+
+			$activeSensors = '';
+            foreach ($sensors as $sensor) {
+                $sensorID = $sensor['VariableID'];
+                $activeSensors .= '- ' . IPS_GetLocation($sensorID) . "\n";
+            }
+            if ($activeSensors == '') {
+                IPS_SetHidden($this -> GetIDForIdent('ActiveSensors'), true);
+                return;
+            }
+
+            $this -> SetValue('ActiveSensors', $activeSensors);
+            IPS_SetHidden($this -> GetIDForIdent('ActiveSensors'), false);
+		}
 		
 		public function RequestAction($Ident, $Value)
 		{
